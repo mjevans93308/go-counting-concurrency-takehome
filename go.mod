acd3d7a23d05164db2d54acae97d3d989f30a103
Matthew Evans <me@mjevans.dev>
2023-08-10 20:05:39 -0600

    Addressed bug where program would return before all goroutines were finished executing using sync.WaitGroup

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
 module github.com/mailgun/mailgun_mjevan93308
 
-go 1.17
+go 1.21
--- integers.go
+++ integers.go
@@ -3,39 +3,58 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"sync"
 
 	"github.com/mailgun/mailgun_mjevan93308/util"
 )
 
 type Result struct {
+	mu     sync.Mutex
 	Even   []int   `json:"even"`
 	Odd    []int   `json:"odd"`
 	Errors []error `json:"errors,omitempty"`
 }
 
+func (result *Result) AddToEven(input int) {
+	result.mu.Lock()
+	defer result.mu.Unlock()
+	result.Even = append(result.Even, input)
+}
+
+func (result *Result) AddToOdd(input int) {
+	result.mu.Lock()
+	defer result.mu.Unlock()
+	result.Odd = append(result.Odd, input)
+}
+
 func main() {
 	result := Result{}
-
-	// create buffered bool chan of size 10
-	s := make(chan bool, 10)
+	// allocate a buffered chan to control the number of go routines active in parallel
+	// use a waitGroup to ensur all goroutines finish before we return the result
+	buff := make(chan struct{}, 10)
+	var wg sync.WaitGroup
 	for i := 0; i <= 100; i++ {
-		// for every iteration until the chan reaches capacity, add a val to it
-		s <- true
-
+		// add capacity to the waitGroup for the current iteration
+		// for every iteration until the buffered chan reaches capacity, add a val to it
+		wg.Add(1)
+		buff <- struct{}{}
 		// re-allocating looping iter var due to unsafe access within go func
 		// https://pkg.go.dev/golang.org/x/tools/go/analysis/passes/loopclosure
-		iter := i
-		go func() {
-			if err := worker(&result, iter); err != nil {
-				fmt.Printf("Encountered error when processing %d: %s", iter, err)
-			}
+		i := i
 
-			// pop bool val off buffered chan once job is done
+		go func() {
+			defer wg.Done()
+			if err := worker(&result, i); err != nil {
+				fmt.Printf("Encountered error when processing %d: %s", i, err)
+			}
+			// pop val off buffered chan once job is done
 			// allowing next job to kick off
-			<-s
+			<-buff
 		}()
+		wg.Wait()
 	}
-	result_json, err := json.Marshal(result)
+
+	result_json, err := json.Marshal(&result)
 	if err != nil {
 		fmt.Println("Could not marshal result to json")
 	}
@@ -50,14 +69,15 @@ func worker(result *Result, iter int) error {
 	api := util.NewApi(util.BuildAddr())
 	response, err := api.GetInteger(iter)
 	if err != nil {
+		fmt.Println("error: %s", err)
 		result.Errors = append(result.Errors, err)
 		return err
 	}
 
 	if util.IsEven(response.Value) {
-		result.Even = append(result.Even, response.Value)
+		result.AddToEven(response.Value)
 	} else {
-		result.Odd = append(result.Odd, response.Value)
+		result.AddToOdd(response.Value)
 	}
 	return nil
 }
